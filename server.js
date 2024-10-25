48a34083db13542be1336e971910c74db56d0445
JosephShortt <josephshortt1@gmail.com>
2024-10-25 08:45:25 +0000

    EnteredREADME

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
@@ -10,7 +15,3 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
