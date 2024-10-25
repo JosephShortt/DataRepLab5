8da8f957a0aaf8585c622ed8bcd20c134a67ffe6
JosephShortt <josephshortt1@gmail.com>
2024-10-25 08:55:06 +0000

    Completed exercise 4

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
 
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`Hello ${name} ${surname}`);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
